dba62434930861c01e95c2b8bfeb6016cba216ae
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-28 00:18:48 +0300

    a different animation

--- web/app.js
+++ web/app.js
@@ -3,18 +3,20 @@
   var loc = window.location
     , w = document.documentElement.clientWidth
     , h = document.documentElement.clientHeight
-    , centerX = w / 2
-    , centerY = h / 2
     , eb = new vertx.EventBus(loc.protocol + '//' + loc.hostname + ':' + loc.port + '/eventbus')
+    , colors = ['#dabca6', '#808ca2', '#a1c699']
     , addWord = function(message) {
-      var x = Math.round(Math.random() * w) - 200 // Start location, x
-        , y = Math.round(Math.random() * h) - 50 // Start location, y
-        , startRotation = Math.round(Math.random() * 16) + 352
-        , endRotation = Math.round(Math.random() * 16) + 352
+      var x = Math.round(Math.random() * w) - 100 // Start location, x
+        , y = - Math.round(Math.random() * h) - 100 // Start location, y
+        , opacity = 0.7 + Math.random() * 0.3
+        , startRotation = Math.round(Math.random() * 20) + 350
+        , endRotation = Math.round(Math.random() * 20) + 350
         , text = document.createElement('div');
 
       // Set initial element attributes and content
       text.className = 'word';
+      text.style.color = colors[_.random(0, colors.length - 1)];
+      text.style.opacity = opacity;
       text.style.left = x + 'px';
       text.style.top = y + 'px';
       text.style.webkitTransform = "rotate("+startRotation+"deg)";
@@ -23,10 +25,8 @@
       // On next tick, set the values at the end of the animation.
       // This will start animating the attributes.
       setTimeout(function() {
-        text.className = 'word word-end';
-        text.style.left = (centerX - text.offsetWidth / 2) + 'px';
-        text.style.top = centerY + 'px';
-        text.style.webkitTransform = "scale(0.01) rotate("+endRotation+"deg)";
+        text.style.top = (h + 500)+'px';
+        text.style.webkitTransform = "rotate("+endRotation+"deg)";
       }, 0);
 
       /// Add the element to the document
@@ -35,7 +35,7 @@
       // After 5 seconds, remove from the document
       setTimeout(function() {
         document.body.removeChild(text);
-      }, 5000);
+      }, 7000);
     };
 
   // Wait for event bus to connect
