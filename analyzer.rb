d44ac95e131319822e10d6e72d240ea5d83df399
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-27 19:49:34 +0300

    stopword filtering

--- analyzer.rb
+++ analyzer.rb
@@ -1,7 +1,21 @@
 require "vertx"
+require "set"
+include Vertx
 
-Vertx::EventBus.register_handler 'tweets' do |tweet|
-  tweet.body.downcase.gsub(/\W/, ' ').split.each do |word|
-    Vertx::EventBus.publish 'tweetWords', word
+STOPWORDS = Set.new(FileSystem.read_file_as_buffer_sync('stopwords.txt').to_s.split("\n"))
+
+def words(tweet)
+  tweet.downcase.split.map { |w| w.gsub /[^\w']/, '' }
+end
+
+def filter_interesting(words)
+  words.select do |word|
+    word.size >= 3 && !STOPWORDS.include?(word) && /^http/ !~ word
+  end
+end
+
+EventBus.register_handler 'tweets' do |tweet|
+  filter_interesting(words(tweet.body)).each do |word|
+    EventBus.publish 'tweetWords', word
   end
 end
