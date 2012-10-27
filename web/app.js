34c213f2442d7a438e53cac344fe3dac4f439f74
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-27 21:04:21 +0300

    Raphaël canvas

---
+++ web/app.js
@@ -0,0 +1,10 @@
+(function() {
+ 
+  var paper = Raphael(0, 0, '100%', '100%');
+  paper.text(100, 100, 'Hello').attr({
+    'font-family': 'Julius Sans One',
+    'font-size': '60px',
+    fill: 'white'
+  });
+
+})();
